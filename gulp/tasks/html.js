1dff057a2a21a5d444b6f9c03840a08801472934
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-07-05 17:51:19 +0300

    update

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,13 +1,13 @@
-// для склейки итогового html файла ир разных кусочков html (хедер футер и т.п.);
+// для склейки итогового html файла из разных кусочков html (хедер футер и т.п.);
 import fileinclude from "gulp-file-include";
 // запрещает кеширование в браузере для определенных файлов
 import versionNumber from "gulp-version-number";
 
-// функция html которая выполняет:
+// функция html, которая выполняет:
 // берет исходный html файл из src, при возникновении ошибки формирует вывод об ошибке, склейка,
-// замена на правильный путь к картинке в итоговом html, с помощью регулярных выражений;
+// замена на правильный путь к картинке в итоговом html с помощью регулярных выражений;
 // запрещает кеширование в браузере для определенных файлов
-// транпортирует из src в корень проекта (см path.js -> объект path)
+// транспортирует из src в корень проекта (см. path.js -> объект path)
 // обновляет браузер при изменениях в исходниках html
 export const html = () => {
   return app.gulp
@@ -17,18 +17,15 @@ export const html = () => {
     .pipe(app.plugins.replace(/@img\//g, "./dist/img/"))
     .pipe(app.plugins.replace(/@video\//g, "./dist/videos/"))
     .pipe(
-      app.plugins.if(
-        app.isProduct,
-        versionNumber({
-          value: "%DT%",
-          append: {
-            key: "_v",
-            cover: 0,
-            to: ["css", "js"],
-          },
-          output: { file: "gulp/version.json" },
-        })
-      )
+      versionNumber({
+        value: "%DT%",
+        append: {
+          key: "_v",
+          cover: 0,
+          to: ["css", "js"],
+        },
+        output: { file: "gulp/version.json" },
+      })
     )
     .pipe(app.gulp.dest(app.path.dist.html))
     .pipe(app.plugins.browsersync.stream());
