0593d38c2468b52262b3a870198aabc4812bda60
Alex <martmarchmartmarch@gmail.com>
2024-07-13 12:52:04 +0300

    add imgs prevew for videos

--- src/js/aboutCompany.js/setVideoPath.js
+++ src/js/aboutCompany.js/setVideoPath.js
@@ -4,10 +4,13 @@ const video = document.querySelector(".video__about-company");
 function setVideoPath(lang) {
   console.log(lang);
   if (lang === "ru") {
+    video.poster = "./dist/img/About/preview-rus.jpg";
     videoSource.src = "./dist/videos/preview-rus.mp4";
   } else if (lang === "uk") {
+    video.poster = "./dist/img/About/preview-rus.jpg";
     videoSource.src = "./dist/videos/preview-ukr.mp4";
   } else {
+    video.poster = "./dist/img/About/preview-eng.jpg";
     videoSource.src = "./dist/videos/preview-eng.mp4";
   }
   video.load();
