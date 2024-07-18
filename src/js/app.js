1b70793581fe651df5a827bc1c4a1f3a0cb839bc
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-07-18 14:21:00 +0300

    add modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 import * as header from "./header/header.js";
 import * as teamSlider from "./teamSlider/teamSlidaer.js";
 import * as i18next from "./i18next/i18next.js";
+
+const animatedModal = document.getElementById("animatedModal");
+const openAnimatedModal = document.querySelector(".messenger-link.weechat");
+const closeAnimatedModal = document.getElementById("closeAnimatedModal");
+
+openAnimatedModal.addEventListener("click", (event) => {
+  event.preventDefault();
+  animatedModal.showModal();
+  animatedModal.style.opacity = "1";
+});
+
+closeAnimatedModal.addEventListener("click", () => {
+  animatedModal.style.opacity = "0";
+  setTimeout(() => animatedModal.close(), 500);
+});
